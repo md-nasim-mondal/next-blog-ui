f0859ca4dd2ea698dfdd3ee4fa8da0eb16aee547
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-23 22:01:52 +0600

    complete the server actions function for create a blog

--- src/actions/create.ts
+++ src/actions/create.ts
@@ -1,4 +1,31 @@
-"use server"
+"use server";
+
+import { redirect } from "next/navigation";
+
 export const create = async (data: FormData) => {
-    console.log(data);
-}
+  const blogInfo = Object.fromEntries(data.entries());
+  const modifiedData = {
+    ...blogInfo,
+    authorId: 1,
+    tags: blogInfo.tags
+      .toString()
+      .split(",")
+      .map((tag) => tag.trim()),
+  };
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(modifiedData),
+  });
+
+  const result = await res.json();
+
+  if (result) {
+    redirect("/blogs");
+  }
+
+  return result;
+};
