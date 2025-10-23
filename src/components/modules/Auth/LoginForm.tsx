98b56d59837a7bd888c192487c262f818da4770b
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-24 01:36:53 +0600

    implement social google login

--- src/components/modules/Auth/LoginForm.tsx
+++ src/components/modules/Auth/LoginForm.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import React from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -14,6 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 type LoginFormValues = {
   email: string;
@@ -34,29 +33,31 @@ export default function LoginForm() {
 
   const handleSocialLogin = (provider: "google" | "github") => {
     console.log(`Login with ${provider}`);
+    signIn(provider, {
+      callbackUrl: "/dashboard",
+    });
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <div className="space-y-6 w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+    <div className='flex justify-center items-center min-h-screen bg-gray-50'>
+      <div className='space-y-6 w-full max-w-md bg-white p-8 rounded-lg shadow-md'>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-6 w-full max-w-md"
-          >
-            <h2 className="text-3xl font-bold text-center">Login</h2>
+            className='space-y-6 w-full max-w-md'>
+            <h2 className='text-3xl font-bold text-center'>Login</h2>
 
             {/* Email */}
             <FormField
               control={form.control}
-              name="email"
+              name='email'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
-                      type="email"
-                      placeholder="Enter your email"
+                      type='email'
+                      placeholder='Enter your email'
                       {...field}
                     />
                   </FormControl>
@@ -68,14 +69,14 @@ export default function LoginForm() {
             {/* Password */}
             <FormField
               control={form.control}
-              name="password"
+              name='password'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
                     <Input
-                      type="password"
-                      placeholder="Enter your password"
+                      type='password'
+                      placeholder='Enter your password'
                       {...field}
                     />
                   </FormControl>
@@ -84,29 +85,28 @@ export default function LoginForm() {
               )}
             />
 
-            <Button type="submit" className="w-full mt-2">
+            <Button type='submit' className='w-full mt-2'>
               Login
             </Button>
 
-            <div className="flex items-center justify-center space-x-2">
-              <div className="h-px w-16 bg-gray-300" />
-              <span className="text-sm text-gray-500">or continue with</span>
-              <div className="h-px w-16 bg-gray-300" />
+            <div className='flex items-center justify-center space-x-2'>
+              <div className='h-px w-16 bg-gray-300' />
+              <span className='text-sm text-gray-500'>or continue with</span>
+              <div className='h-px w-16 bg-gray-300' />
             </div>
           </form>
         </Form>
         {/* Social Login Buttons */}
-        <div className="flex flex-col gap-3 mt-4">
+        <div className='flex flex-col gap-3 mt-4'>
           <Button
-            variant="outline"
-            className="flex items-center justify-center gap-2"
-            onClick={() => handleSocialLogin("github")}
-          >
+            variant='outline'
+            className='flex items-center justify-center gap-2'
+            onClick={() => handleSocialLogin("github")}>
             {/* GitHub */}
             <Image
-              src="https://img.icons8.com/ios-glyphs/24/github.png"
-              alt="GitHub"
-              className="w-5 h-5"
+              src='https://img.icons8.com/ios-glyphs/24/github.png'
+              alt='GitHub'
+              className='w-5 h-5'
               width={20}
               height={20}
             />
@@ -114,24 +114,23 @@ export default function LoginForm() {
           </Button>
 
           <Button
-            variant="outline"
-            className="flex items-center justify-center gap-2"
-            onClick={() => handleSocialLogin("google")}
-          >
+            variant='outline'
+            className='flex items-center justify-center gap-2'
+            onClick={() => handleSocialLogin("google")}>
             {/* Google */}
             <Image
-              src="https://img.icons8.com/color/24/google-logo.png"
-              alt="Google"
-              className="w-5 h-5"
+              src='https://img.icons8.com/color/24/google-logo.png'
+              alt='Google'
+              className='w-5 h-5'
               width={20}
               height={20}
             />
             Login with Google
           </Button>
         </div>
-        <p className="text-center text-sm text-gray-500 mt-4">
+        <p className='text-center text-sm text-gray-500 mt-4'>
           Don’t have an account?{" "}
-          <Link href="/register" className="text-blue-500 hover:underline">
+          <Link href='/register' className='text-blue-500 hover:underline'>
             Register
           </Link>
         </p>
